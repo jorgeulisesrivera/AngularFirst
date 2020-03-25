eed69b6d97009aba03293f9454e55e72caf37544
jorge <reservado.cl@gmail.com>
2020-03-25 16:54:23 -0300

    primeros cambios en un proyecto angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from '../app/components/header/header.component'
+import { BodyComponent } from '../app/components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component'
 
 @NgModule({
   declarations: [
-    AppComponent,HeaderComponent
+    AppComponent,HeaderComponent,BodyComponent, FooterComponent
   ],
   imports: [
     BrowserModule
